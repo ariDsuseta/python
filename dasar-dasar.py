9dd498fc0e54a2c63a0ce2f06f5edfb08c9d5808
Ari Suseta <itsukaakatsuki@gmail.com>
2025-03-19 22:06:33 +0700

    belajar dasar dasar python

---
+++ dasar-dasar.py
@@ -0,0 +1,13 @@
+# print("Hallo World") mencetak kelayar
+
+# variabel dan tipe data
+
+nama = "ari suseta" # string
+umur = 29 # integer
+tinggi = 168,8 # float
+belajar = True # boolean
+
+print(type(nama))
+print(type(umur))
+print(type(tinggi))
+print(type(belajar))
