ce074b8cc336c28e081c3dadaad91c46d11835ac
Ari Suseta <itsukaakatsuki@gmail.com>
2025-03-19 22:22:41 +0700

    konfersi tipe data

--- dasar-dasar.py
+++ dasar-dasar.py
@@ -12,9 +12,19 @@
 # print(type(tinggi))
 # print(type(belajar))
 
-# mengambil input dari pengguna
+# mengambil input dari pengguna => selalu menghasilkan string
 
-nama = input("Masukan Nama anda: ")
-umur = input("Masukan umur anda: ")
+# nama = input("Masukan Nama anda: ")
+# umur = input("Masukan umur anda: ")
+#
+# print(f"Nama: {nama}, Umur: {umur}")
 
-print(f"Nama: {nama}, Umur: {umur}")
+# confersi tipe data
+
+angka1 = int(input("Masukan angka pertama: "))
+angka2 = int(input("Masukan angka kedua: "))
+
+# print(f"Angka pertama: {angka1} \nangka kedua: {angka2}")
+
+hasil = angka1 + angka2
+print(f"Hasil dari penjumblahan {angka1} + {angka2} = {hasil}")
