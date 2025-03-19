c1f3e944e4d8f4c012fe55a5fd591091b97b8c77
Ari Suseta <itsukaakatsuki@gmail.com>
2025-03-19 22:17:01 +0700

    input user

--- dasar-dasar.py
+++ dasar-dasar.py
@@ -2,12 +2,19 @@
 
 # variabel dan tipe data
 
-nama = "ari suseta" # string
-umur = 29 # integer
-tinggi = 168,8 # float
-belajar = True # boolean
+# nama = "ari suseta" # string
+# umur = 29 # integer
+# tinggi = 168,8 # float
+# belajar = True # boolean
+#
+# print(type(nama))
+# print(type(umur))
+# print(type(tinggi))
+# print(type(belajar))
 
-print(type(nama))
-print(type(umur))
-print(type(tinggi))
-print(type(belajar))
+# mengambil input dari pengguna
+
+nama = input("Masukan Nama anda: ")
+umur = input("Masukan umur anda: ")
+
+print(f"Nama: {nama}, Umur: {umur}")
